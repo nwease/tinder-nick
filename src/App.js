60e24284660e72366a340af2f00737a232645ff8
nwease <nickwease@gmail.com>
2020-09-25 11:16:41 +0200

    Working on Swipe Buttons fixing styling f ups.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Cards from './components/Cards';
+import Swipe from './components/Swipe';
 
 function App() {
     return (
         <div className='app'>
             <Header />
             <Cards />
-
-            {/*SWIPE*/}
+            <Swipe />
         </div>
     );
 }
